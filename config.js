f51118b8fbce2a820bef0972257b1b3fa3304021
kasora <kasorasun@gmail.com>
2017-03-31 22:51:59 +0800

    fix config.

---
+++ config.js
@@ -0,0 +1,18 @@
+"use strict"
+
+module.exports = {
+    port: 12450,
+    dbUrl: 'mongodb://localhost:27017/kasora',
+    urls: 'urls',
+    user: 'user',
+    token: 'token',
+
+    dataWay: "mongodb",
+    log: "console",
+
+    adminEamil: "test@test.com",
+    adminPassword: "testpassword",
+    
+    disposeTime: 15,
+    renewTime: 1
+}
